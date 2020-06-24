fc8fd40f1913659c2f56f29017fd1a8fcd5999db
Kaizen Conroy <kcc2@williams.edu>
2020-06-24 12:13:38 -0400

    added dependencies

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -3,6 +3,7 @@ import * as codepipeline from '@aws-cdk/aws-codepipeline';
 import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions';
 import * as cdk from '@aws-cdk/core';
 import * as codebuild from '@aws-cdk/aws-codebuild';
+import * as s3 from '@aws-cdk/aws-s3';
 import * as iam from '@aws-cdk/aws-iam';
 
 
@@ -16,19 +17,35 @@ export class PipelineStack extends cdk.Stack {
         //     changeSetName: 'pipelineChangeSet',
         // });
 
+        const bucket = new s3.Bucket(this, 'ArtifactBucket', {
+            versioned: true,
+            removalPolicy: cdk.RemovalPolicy.DESTROY,
+          });
+
+        const role = new iam.Role(this, 'ActionRole', {
+        assumedBy: new iam.AccountPrincipal(cdk.Aws.ACCOUNT_ID),
+        });
+
+        role.addToPolicy(new iam.PolicyStatement({
+            actions: ['s3:*'],
+            resources: ['*'],
+          }));
+
         // Create a pipeline
-        const pipeline = new codepipeline.Pipeline(this, 'MyPipeline');
+        const pipeline = new codepipeline.Pipeline(this, 'MyPipeline',{
+            artifactBucket: bucket,
+        });
 
         // Related to source action
         const sourceOutput = new codepipeline.Artifact();
         const sourceAction = new codepipeline_actions.GitHubSourceAction({
-        actionName: 'Github_Source',
-        owner: 'kaizen3031593',
-        repo: 'coal-mine',
-        oauthToken: cdk.SecretValue.secretsManager('GithubToken'),
-        output: sourceOutput,
-        branch: 'master',
-        trigger: codepipeline_actions.GitHubTrigger.POLL,
+            actionName: 'Github_Source',
+            owner: 'kaizen3031593',
+            repo: 'coal-mine',
+            oauthToken: cdk.SecretValue.secretsManager('GithubToken'),
+            output: sourceOutput,
+            branch: 'master',
+            trigger: codepipeline_actions.GitHubTrigger.POLL,
         });
 
         const pipelineStack = cdk.Stack.of(this);
@@ -65,18 +82,19 @@ export class PipelineStack extends cdk.Stack {
 
         const projectBuildOutput = new codepipeline.Artifact('ProjectBuildOutput');
         const buildAction = new codepipeline_actions.CodeBuildAction({
-        actionName: 'Project_Build',
-        input: sourceOutput,
-        outputs: [projectBuildOutput],
-        project: project,
+            actionName: 'Project_Build',
+            input: sourceOutput,
+            outputs: [projectBuildOutput],
+            project: project,
         });
         
         // Related to deployment
         const deployAction = new codepipeline_actions.CloudFormationCreateUpdateStackAction({
-            actionName: 'Deploy',
+            actionName: 'CFN_Deploy',
             adminPermissions: true,
             stackName: 'deploy',
-            templatePath: projectBuildOutput.atPath(this.templateFile),            
+            templatePath: projectBuildOutput.atPath(this.templateFile),    
+            role,       
         });
 
         // Add stages to pipeline
@@ -94,7 +112,7 @@ export class PipelineStack extends cdk.Stack {
         });
 
         pipeline.artifactBucket.grantRead(deployAction.deploymentRole);
-        
+        console.log(this.templateFile);
     }
 }
 
