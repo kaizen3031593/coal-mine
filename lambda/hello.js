57bf0c55c33ddf38f2f0d1dc3fe8c679752b7d38
Kaizen Conroy <kcc2@williams.edu>
2020-07-08 11:47:45 -0400

    changed lambda function

--- lambda/hello.js
+++ lambda/hello.js
@@ -1,19 +1,30 @@
 exports.handler = async function(event) {
-    let name = 'you';
-    let food = 'butternut squash';
-    if(event.queryStringParameters && event.queryStringParameters.name) {
-        name = event.queryStringParameters.name;
-    }
-    if(event.queryStringParameters && event.queryStringParameters.food) {
-        food = event.queryStringParameters.food;
-    }
-    console.log("request:", JSON.stringify(event, undefined, 2));
-    let greeting = `Hello, ${name}! I also like to eat ${food}!`;
+    message = new Date(Date.now()).toLocaleString();
     let response = {
         statusCode: 200,
         headers: { "Content-Type": "text/plain" },
-        body: greeting
+        body: message
     };
-    console.log("response: " + JSON.stringify(response));
     return response;
-  };
+};
+
+// exports.handler = async function(event) {
+//     let name = 'you';
+//     let food = 'butternut squash';
+//     if(event.queryStringParameters && event.queryStringParameters.name) {
+//         name = event.queryStringParameters.name;
+//     }
+//     if(event.queryStringParameters && event.queryStringParameters.food) {
+//         food = event.queryStringParameters.food;
+//     }
+//     console.log("request:", JSON.stringify(event, undefined, 2));
+//     let greeting = `Hello, ${name}! I also like to eat ${food}!`;
+//     let response = {
+//         statusCode: 200,
+//         headers: { "Content-Type": "text/plain" },
+//         body: greeting
+//     };
+//     console.log("response: " + JSON.stringify(response));
+//     return response;
+//   };
+
