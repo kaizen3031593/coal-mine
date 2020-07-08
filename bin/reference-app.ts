732dc88718691cff1d01efb9969e55fe99e7d8f2
Kaizen Conroy <kcc2@williams.edu>
2020-07-08 11:30:16 -0400

    made modifications

--- bin/reference-app.ts
+++ bin/reference-app.ts
@@ -3,14 +3,13 @@ import * as cdk from '@aws-cdk/core';
 import { ReferenceAppStack } from '../lib/reference-app-stack';
 import { PipelineStack } from '../lib/pipeline-stack';
 import { CanaryStack } from '../lib/canary-stack';
-import { CanaryL2Stack } from '../lib/canary-l2-stack';
+// import { CanaryL2Stack } from '../lib/canary-l2-stack';
 
 const app = new cdk.App();
 const lambdaStack = new ReferenceAppStack(app, 'LambdaStack');
 new PipelineStack(app, 'PipelineStack', {
     lambdaCode: lambdaStack.lambdaCode,
 });
-new CanaryStack(app, 'CanaryStack');
-new CanaryL2Stack(app, 'CanaryL2Stack');
-
+//new CanaryStack(app, 'CanaryStack');
+//new CanaryL2Stack(app, 'CanaryL2Stack');
 app.synth();
--- lib/canary-l2-stack.ts
+++ lib/canary-l2-stack.ts
@@ -1,69 +1,17 @@
 import * as cdk from '@aws-cdk/core';
-import { Canary, Code } from '@aws-cdk/aws-synthetics';
+import { Canary, Code, Rate } from '@aws-cdk/aws-synthetics';
 import * as cloudwatch from '@aws-cdk/aws-cloudwatch';
 
 export class CanaryL2Stack extends cdk.Stack {
     constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
-
         const canary = new Canary(this, 'my_test', {
           handler: 'index.handler',
           canaryName: 'myl2canary',
-          code: Code.fromInline(`var synthetics = require('Synthetics');
-          const log = require('SyntheticsLogger');
-          const https = require('https');
-          const http = require('http');
-          
-          const apiCanaryBlueprint = async function () {
-              const postData = "";
-          
-              const verifyRequest = async function (requestOption) {
-                return new Promise((resolve, reject) => {
-                  log.info("Making request with options: " + JSON.stringify(requestOption));
-                  let req
-                  if (requestOption.port === 443) {
-                    req = https.request(requestOption);
-                  } else {
-                    req = http.request(requestOption);
-                  }
-                  req.on('response', (res) => {
-                    log.info(\`Status Code: \${res.statusCode}\`)
-                    log.info(\`Response Headers: \${JSON.stringify(res.headers)}\`)
-                    if (res.statusCode !== 200) {
-                       reject("Failed: " + requestOption.path);
-                    }
-                    res.on('data', (d) => {
-                      log.info("Response: " + d);
-                    });
-                    res.on('end', () => {
-                      resolve();
-                    })
-                  });
-          
-                  req.on('error', (error) => {
-                    reject(error);
-                  });
-          
-                  if (postData) {
-                    req.write(postData);
-                  }
-                  req.end();
-                });
-              }
-          
-              const headers = {}
-              headers['User-Agent'] = [synthetics.getCanaryUserAgentString(), headers['User-Agent']].join(' ');
-              const requestOptions = {"hostname":"ajt66lp5wj.execute-api.us-east-1.amazonaws.com","method":"GET","path":"/prod/","port":443}
-              requestOptions['headers'] = headers;
-              await verifyRequest(requestOptions);
-          };
-          
-          exports.handler = async () => {
-              return await apiCanaryBlueprint();
-          };`)
+          code: Code.fromAsset('./lambda'),
       });
 
-        const canaryMetric = canary.metricSuccess();
+        const canaryMetric = canary.metricSuccessPercent();
 
         canary.createAlarm('CanaryAlarm2',{
           metric: canaryMetric,
@@ -71,7 +19,5 @@ export class CanaryL2Stack extends cdk.Stack {
           threshold: 99,
           comparisonOperator: cloudwatch.ComparisonOperator.LESS_THAN_THRESHOLD,
         });
-
-        //console.log(canary.canaryState);
     }
 }
